0f82ab83b1d6a8a9f27830f050b0369a7bd236dd
caminspired <charlotteaowusuansah@gmail.com>
2024-01-31 22:32:25 +0000

    1 jav

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+let cIsFun = 'C is fun';
+let pythonIsCool = 'Python is cool';
+let jsIsAmazing = 'JavaScript is amazing';
+
+console.log(cIsFun);
+console.log(pythonIsCool);
+console.log(jsIsAmazing);
