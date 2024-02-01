40b292da61636d80ecf2ed688e691ad97a74b8ae
caminspired <charlotteaowusuansah@gmail.com>
2024-02-01 15:06:36 +0000

    2 jav obj

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
     constructor(w, h) {
-        this.width = w;
-        this.height = h;
         if (w <= 0 || isNaN(w) || h <= 0 || isNaN(h)) {
             return {};
         }
+        this.width = w;
+        this.height = h;
     }
-    
 }
 
 module.exports = Rectangle;
