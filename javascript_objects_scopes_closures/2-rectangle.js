d132864ae3a60888b04b9865ef838274143dd1c0
caminspired <charlotteaowusuansah@gmail.com>
2024-02-01 16:09:37 +0000

    2 jav obj

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
     constructor(w, h) {
-        if (w > 0 || Number.isInteger(w) || h > 0 || Number.isInteger(h)) {};    
-        this.width = w;
-        this.height = h;  
+        if (w > 0 || Number.isInteger(w) || h > 0 || Number.isInteger(h)) {
+            this.width = w;
+            this.height = h;
+        };      
     }
 }
 
