83d2224a4b2e84f33b826fa335ed425dba1a589b
caminspired <charlotteaowusuansah@gmail.com>
2024-02-01 18:03:26 +0000

    6 jav obj

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+
+class Square extends Square {
+    charPrint(c) {
+        if (c === undefined) {
+            c = 'X';
+        }
+        for (let counter_h = 0; counter_h < this.height; counter_h++ ) {
+            let row = '';
+            for (let counter_w = 0; counter_w < this.width; counter_w++) {
+                row += c;
+            }
+            console.log(row);
+        }
+    }
+};
+
+
+
+module.exports = Square;
