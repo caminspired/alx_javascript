d271545111e6d8a9520d2b8346016a114f46cd83
caminspired <charlotteaowusuansah@gmail.com>
2024-02-06 22:01:22 +0000

    4 jav net

---
+++ javascript-network/4-completed_tasks.js
@@ -0,0 +1,31 @@
+const req = require('request');
+
+const apiUrl = process.argv[2];
+
+req.get(apiUrl, (error, response, body) => {
+    if (error) {
+        console.error('error:', error);
+        return;
+    }
+
+    if (response.statusCode !== 200) {
+        console.error('error:', response.body);
+        return;
+    }
+
+    const tasks = JSON.parse(body);
+    const completedTasks = {};
+
+    tasks.forEach(task => {
+        if (task.complete) {
+            if (completedTasks[task.userId]){
+                completedTasks[task.userId]++;
+            }
+            else {
+                completedTasks[task.userId] = 1;
+            }
+        }
+    });
+
+    console.log(completedTasks);
+});
