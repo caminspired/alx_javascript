e66a6ea3e82fa1a1a1008db458dc43c5340f04f8
caminspired <charlotteaowusuansah@gmail.com>
2024-02-06 21:47:06 +0000

    3 jav net

---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,25 @@
+const req = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+req.get(url, (error, response, body) => {
+    if (error) {
+        console.error('error:', error);
+        return;
+    }
+
+    if (response.statusCode !== 200) {
+        console.error('error:', response.body);
+        return;
+    }
+
+    fs.writeFile(filePath, body, {encoding: 'utf-8'}, (err) => {
+        if (err) {
+            console.error('Writing error:', err);
+            return;
+        }
+        console.log('File save successful!');
+    });
+});
