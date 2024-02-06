afb47b718473bdb6ede0a521b88b8c9f7d087d16
caminspired <charlotteaowusuansah@gmail.com>
2024-02-06 20:35:10 +0000

    0 jav net

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -2,7 +2,7 @@ const req = require('request');
 
 const url = process.argv[2];
 
-req.url(url, function(error, response) {
+req.url(url, (error, response) => {
     if (error) {
         console.log('error: ', error);
         return;
