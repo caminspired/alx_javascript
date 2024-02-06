1c75a3290045f430bae01a0e1264216b98480425
caminspired <charlotteaowusuansah@gmail.com>
2024-02-06 21:32:10 +0000

    1 jav net

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,7 +1,7 @@
 const req = require('request');
 
-const movieId = process.argv[1];
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/${movieId}';
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 req.get(apiUrl, (error, response, body) => {
     if (error) {
