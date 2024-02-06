caf9e2a4afaa32511c7c92fe508acb60d6796c38
caminspired <charlotteaowusuansah@gmail.com>
2024-02-06 22:20:05 +0000

    5 jav net

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -3,7 +3,22 @@ const req = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-    req.get(apiUrl, (error, response, body) => {
+req.get(apiUrl, (error, response, body) => {
+    if (error) {
+        console.error('error:', error);
+        return;
+    }
+
+    if (response.statusCode !== 200) {
+        console.error('error:', response.body);
+        return;
+    }
+
+    const movie = JSON.parse(body);
+    const characterNames = movie.characters;
+
+
+    req.get(characterNames, (error, response, body) => {
         if (error) {
             console.error('error:', error);
             return;
@@ -14,22 +29,7 @@ const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
             return;
         }
 
-        const movie = JSON.parse(body);
-        const characterNames = movie.characters;
-
-
-        req.get(characterNames, (error, response, body) => {
-            if (error) {
-                console.error('error:', error);
-                return;
-            }
-    
-            if (response.statusCode !== 200) {
-                console.error('error:', response.body);
-                return;
-            }
-    
-            const character = JSON.parse(body); 
-            console.log(character.name);
-        })
-    });
+        const character = JSON.parse(body); 
+        console.log(character.name);
+    })
+});
