ffbc21c464fb4e74cd970ac80869b7798b53a510
caminspired <charlotteaowusuansah@gmail.com>
2024-02-06 21:34:10 +0000

    2 jav net

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,20 @@
+const req = require('request');
+
+const characterId = '18'
+const apiUrl = process.argv[2];
+
+req.get(apiUrl, (error, response, body) => {
+    if (error) {
+        console.error('error:', error);
+        return;
+    }
+
+    if (response.statusCode !== 200) {
+        console.error('error:', response.body);
+        return;
+    }
+
+    const movies = JSON.parse(body).results;
+    const count = movies.filter(movie => movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
+    console.log(count);
+});
