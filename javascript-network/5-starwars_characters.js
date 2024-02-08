f383f82c9ee27a5c079818ae6fa7e306bc22190f
caminspired <charlotteaowusuansah@gmail.com>
2024-02-08 10:40:01 +0000

    5 jav net

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -3,7 +3,7 @@ const req = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-request(apiUrl, function (_err, _res, body) {
+req(apiUrl, function (_err, _res, body) {
     const characters = JSON.parse(body).characters;
 
     for (let i = 0; i < characters.length; i++) {
