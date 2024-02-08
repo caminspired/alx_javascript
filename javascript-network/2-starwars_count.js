1bf49ba23264ac29585369a6b02b56e25969d599
caminspired <charlotteaowusuansah@gmail.com>
2024-02-08 10:58:58 +0000

    2 jav net

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,27 +1,22 @@
-const req = require('request');
-
-const characterId = '18';
+const request = require('request');
 const apiUrl = process.argv[2];
+const characterId = 18;
+let count = 0;
 
-req.get(apiUrl, (error, response, body) => {
-    if (error) {
-        console.error('error:', error);
-        return;
-    }
-
-    if (response.statusCode !== 200) {
-        console.error('error:', response.body);
-        return;
-    }
-
-    const count = 0;
-    const movies = JSON.parse(body)
-    movies.results.forEach((film) => {
-        film.characters.forEach((character) => {
-            if (character.includes(characterId)){
-                count += 1;
-            }
+request.get(apiUrl, (error, response, body) => {
+    if (error){
+        console.error(error.message);
+    }else if (response.statusCode !== 200){
+        console.error(response.statusCode);
+    }else {
+        const movieData = JSON.parse(body)
+        movieData.results.forEach((film) => {
+            film.characters.forEach((character) => {
+                if (character.includes(characterId)){
+                    count += 1;
+                }
+            })
         })
-    })
-    console.log(count);
+        console.log(count);
+    }
 });
